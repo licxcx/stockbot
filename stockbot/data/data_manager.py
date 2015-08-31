84fcbb5c0f8a3887d53dc9f5f60875cd30e46d99
licx <licxcx@126.com>
2015-08-31 23:09:48 +0800

    Add update_funda method into DataManager

--- stockbot/data/data_manager.py
+++ stockbot/data/data_manager.py
@@ -9,25 +9,28 @@ from stockbot.util import stockdate
 
 
 class DataManager(object):
-
     def __init__(self, db="stock.db"):
         self.fas = FundASpider()
         self.db = db
 
-    def _insert_funda(self):
-        self.fas.insert_funda_data()
+    def update_funda(self, now):
+        if not self._is_record_exists(now):
+            self.fas.insert_funda_data()
+        else:
+            self.fas.update_funda_data()
 
     def _is_record_exists(self, date_time):
         with sqlite3.connect(self.db) as conn:
             cur = conn.cursor()
-            cur.execute('''select distinct date from Afund where date = ?''', (str(date_time.date()),))
+            cur.execute('''select distinct date from Afund where date = ?''',
+                        (str(date_time.date()),))
             return True if cur.fetchall() else False
 
     def update_all_data(self):
         now = datetime.now()
-        if stockdate.is_trading_datetime(now) and not self._is_record_exists(now):
-            self._insert_funda()
-            
+        if (stockdate.is_trading_date(now) and now.hour >= 15
+                or stockdate.is_trading_datetime(now)):
+            self.update_funda(now)
 
 
 if __name__ == '__main__':
